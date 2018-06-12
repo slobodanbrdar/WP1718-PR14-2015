a980b29756af66e976e7e980df52265de63730fc
Slobodan Brdar <slobodan.brdar11@gmail.com>
2018-06-12 14:35:57 +0200

    Napravljen projekat

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        public List<Product> Get()
+        {
+            return Products.ProductsList;
+        }
+
+        public void Post([FromBody]Product product)
+        {
+            product.Id = Products.ProductsList.Count + 1;
+            Products.ProductsList.Add(product);
+        }
+    }
+}
---
+++ WebAPI/Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Products
+    {
+        public static List<Product> ProductsList { get; set; } = new List<Product>()
+        {
+            new Product() { Id = 1, Name = "Joovan" },
+            new Product() { Id = 2, Name = "Solja" },
+            new Product() { Id = 3, Name = "Marker" }
+        };
+    }
+}
